e1c110685155bb1033b1e1fb06f38b64633c9769
Lynnn8 <2235897647@qq.com>
2024-08-16 16:22:48 +0800

    feat/20240816/开发order页面

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -1,66 +1,14 @@
 // pages/order/order.js
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad(options) {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload() {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh() {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom() {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage() {
-
-	}
-})
+  data: {
+    tickets: [
+      { name: '普通票 (不限人群)', heightLimit: '120cm以上', price: 1080 },
+      { name: '普通票 (双人票)', heightLimit: '120cm以上', price: 2150 }
+    ]
+  },
+  purchase: function(event) {
+    const index = event.currentTarget.dataset.index;
+    console.log('您选择了：', this.data.tickets[index].name);
+    // 这里可以添加购买票务的逻辑，例如跳转到订单确认页面
+  }
+});
